5e02c1d0f3d8cfc2f5aa21f907268ce0df170e08
jiindev <jiindev@gmail.com>
2020-02-13 06:20:34 +0900

    랜딩 페이지에 비디오들 나타나게 하기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,62 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
+import {Card, Icon, Avatar, Col, Typography, Row} from 'antd';
+import Axios from 'axios';
+import moment from 'moment';
+const {Title} = Typography;
+const {Meta} = Card;
 
-function LandingPage() {
+
+const LandingPage = () => {
+    const [Video, setVideo] = useState([])
+
+    useEffect(() => {
+        Axios.get('/api/video/getVideos')
+        .then(response => {
+            if(response.data.success){
+                console.log(response.data);
+                setVideo(response.data.videos);
+            }else{
+                console.log('비디오 가져오기를 실패했습니다.');
+            }
+        })
+    }, [])
+
+    const renderCards = Video.map((video, index)=>{
+        let minutes = Math.floor(video.duration/60);
+        let seconds = Math.floor((video.duration-minutes * 60));
+        return <Col lg={6} md={8} xs={24}>
+                    <a href={`/video/post/${video._id}`}>
+                        <div style={{position:'relative'}}>
+                            <img style={{width:'100%'}}src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail"/>
+                            <div className="duration">
+                                <span>{minutes} : {seconds}</span>
+                            </div>
+                        </div>
+                    </a>
+                    <br/>
+                    <Meta
+                    avatar={<Avatar src={video.writer.image}/>}
+                    title={video.title}
+                    description="">
+                    </Meta>
+                    <span>{video.writer.name}</span>
+                    <span style={{maginLeft: '3rem'}}>{video.views} views</span>
+                    - <span>{moment(video.createAt).format("MMM Do YYY")}</span>
+        </Col>
+    });
     return (
         <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-        </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
+       <div style={{width: '85%', margin: '3rem auto'}}>
+            <Title level={2}>Recommended</Title>
+            <hr/>
+            <Row gutter={16}>
+
+                
+                {renderCards}
+                
+            </Row>
+       </div>
         </>
     )
 }
